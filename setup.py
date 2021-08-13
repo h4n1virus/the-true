491c9ba742978fcfe35556d4caacf8c3f281f524
Sander Viirmaa <h4n1virus@gmail.com>
2021-08-13 09:51:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: WTFPL License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
