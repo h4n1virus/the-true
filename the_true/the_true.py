834b93571ba3f5183fd029e77d648825b3300a05
Sander Viirmaa <h4n1virus@gmail.com>
2021-08-13 10:37:10 +0300

    little speedboost

--- the_true/the_true.py
+++ the_true/the_true.py
@@ -15,10 +15,13 @@ class __THE_TRUE(object):
         return randint(self.__minimum_value, self.__maximum_value)
 
     def __is_prime(self, number):
-        for i in range(self.__minimum_value, number):
+        for i in range(self.__minimum_value, int(number ** 0.5)):
             if (number % i) == 0:
                 return 0 == 1
         return 1 == 1
+
+    def is_prime_primality_test(self, number):
+        pass
 
     def __call__(self):
         random_number = self.__random_number()
